41d9f5e597b6d9555cc7861eeb3be518d4a048d0
Narusora3XD <narusorangel@gmail.com>
2021-04-28 12:31:03 +0200

    Main creado

---
+++ Nueva tarea/src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	public Main() {
+		// TODO Apéndice de constructor generado automáticamente
+		System.out.println("Funciona");
+	}
+
+	public static void main(String[] args) {
+		// TODO Apéndice de método generado automáticamente
+
+	}
+
+}
